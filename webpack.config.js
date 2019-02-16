05975b5b82c8b5bba14ee932cf3f5f83411bbb70
Naod Yeheyes <naodya@gmail.com>
2019-02-16 13:21:59 -0500

    Updated webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   plugins: [htmlWebpackPlugin],
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['.js', '.jsx'],
   },
   devServer: {
     port: 3001
